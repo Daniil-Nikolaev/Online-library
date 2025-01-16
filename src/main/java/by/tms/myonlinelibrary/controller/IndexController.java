192524d349973df876c6ee400e549625c88c207a
Daniil <daniillalala14@gmail.com>
2025-01-16 16:45:04 +0300

    Edit HTML pages

--- src/main/java/by/tms/myonlinelibrary/controller/IndexController.java
+++ src/main/java/by/tms/myonlinelibrary/controller/IndexController.java
@@ -1,15 +1,25 @@
 package by.tms.myonlinelibrary.controller;
 
+import by.tms.myonlinelibrary.entity.Book;
+import by.tms.myonlinelibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @Autowired
+    BookService bookService;
 
     @GetMapping
-    public String greeting() {
+    public String greeting(Model model) {
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
         return "homepage";
     }
 
